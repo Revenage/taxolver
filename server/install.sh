39a100afa3ec4f0074317f480fb3b93ebdf38ba5
Revenage <romanvolobuev93@gmail.com>
2018-08-31 07:36:32 +0300

    reload

--- server/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python3 -m venv myenv
-source myenv/bin/activate
-python3 -m pip install --upgrade pip
-pip3 install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
